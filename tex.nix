80b40187a12af98961077a0a7fc37ab2f472a75f
Zhenbo Li <lizhenbo@yahoo.com>
2023-11-10 09:14:19 -0500

    added tex

--- tex.nix
+++ tex.nix
@@ -1,14 +1,12 @@
  let
-   nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-22.11";
+   nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-23.05";
    pkgs = import nixpkgs { config = { allowUnsupportedSystem = true; }; overlays = []; };
-   # nixpkgs.config.allowUnsupportedSystem = true;
-   # allowUnsupportedSystem = true;
  in
   pkgs.mkShell {
-    # NIXPKGS_ALLOW_UNSUPPORTED_SYSTEM=1;
+
     nativeBuildInputs = with pkgs.buildPackages; [ 
       texstudio
-
+      texlive.combined.scheme-full
     ];
 
 }
