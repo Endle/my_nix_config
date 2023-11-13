92877d4ff5104024b1af07de6fb6f1b348f617e3
Zhenbo Li <lizhenbo@yahoo.com>
2023-11-13 12:01:51 -0500

    finished install spectrum_util

--- ms.nix
+++ ms.nix
@@ -5,6 +5,21 @@ let
     pandas
     jupyter
     rdkit
+    (
+      buildPythonPackage rec {
+        pname = "spectrum_utils";
+        version = "0.4.2";
+        src = fetchPypi {
+          inherit pname version;
+          sha256 = "sha256-egIS8StncMWSGE9OgvS3ux8RC02yo7kUobSy8/jHlpM=";
+        };
+        doCheck = false;
+        propagatedBuildInputs = [
+          # Specify dependencies
+          #pkgs.python3Packages.numpy
+        ];
+      }
+    )
   ];
   my-python = pkgs.python3.withPackages my-python-packages;
 in my-python.env
