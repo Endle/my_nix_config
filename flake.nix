d5af78ba6f47cf4591069322c976cbe9d37708cb
Zhenbo Li <lizhenbo@yahoo.com>
2024-09-15 10:23:58 -0400

    add tree sitter

--- flake.nix
+++ flake.nix
@@ -39,6 +39,7 @@
         pkgs.rustup
 
         pkgs.neovim
+        pkgs.tree-sitter
         pkgs.ctags
 
         pkgs.qemu
@@ -61,17 +62,14 @@
             pkgs.libreoffice-bin
           pkgs.inkscape
             pkgs.mpv
-        ];
+    ];
 
 
-      # Auto upgrade nix package and the daemon service.
+
       services.nix-daemon.enable = true;
-      # nix.package = pkgs.nix;
 
-      # Necessary for using flakes on this system.
       nix.settings.experimental-features = "nix-command flakes";
 
-      # Create /etc/zshrc that loads the nix-darwin environment.
       programs.zsh.enable = true;  # default shell on catalina
       # programs.fish.enable = true;
 
